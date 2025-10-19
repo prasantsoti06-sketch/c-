7b03a5647a645017fce79f8568f7261295bd89df
Prasahnt soti <prasantsoti06@gmail.com>
2025-10-19 16:48:08 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks.Dataflow;
+namespace cshsarp
+{
+     class code
+    {
+        static void mymethod()
+        {
+            int a = 4;
+            int b = 7;
+
+            Console.WriteLine(a + b);
+        }
+        static void Main(string[] args)
+        {
+                  //if, else if, else condition
+            // Console.WriteLine("Enter your marks:");
+            // int marks = Convert.ToInt32(Console.ReadLine());
+            // if (marks >= 90 && marks <= 100)
+            // {
+            //     Console.WriteLine("A+");
+
+            // }
+            // else if (marks >= 70 && marks <= 80)
+            // {
+            //     Console.WriteLine("your marks is:A");
+            // }
+            // else if (marks >= 50 && marks <= 60)
+            // {
+            //     Console.WriteLine("D+");
+            // }
+
+            // else
+            // {
+            //     Console.WriteLine("NG");
+            // }
+            // swtich casse
+            // Console.WriteLine("Please enter day:");
+            // int day = Convert.ToInt32(Console.ReadLine());
+            //      switch(day)
+            // {
+            //     case 1:
+            //         Console.WriteLine("sunday");
+            //         break;
+
+            //     case 2:
+            //         Console.WriteLine("Monday");
+            //         break;
+
+            //     case 3:
+            //         Console.WriteLine("Tuesday");
+            //         break;
+
+            //     case 4:
+            //         Console.WriteLine("wednesday");
+            //         break;
+
+            //     case 5:
+            //         Console.WriteLine("thuesday");
+            //         break;
+
+            //     case 6:
+            //         Console.WriteLine("friday");
+            //         break;
+
+            //         case 7:
+            //         Console.WriteLine("saturday");
+            //         break;
+
+            //     default:
+            //         Console.WriteLine("there is only 7 days in a one week so give input between 1 to 7\tThanks ");
+            //         break;
+            // }
+
+                    // // by if else statement
+            // Console.WriteLine("enter a day number:");
+            // int day = Convert.ToInt32(Console.ReadLine());
+
+            // if (day > 0 && day < 2)
+            // {
+            //     Console.WriteLine("sunday");
+            // }
+            // else if (day > 1 && day < 3)
+            // {
+            //     Console.WriteLine("monday");
+            // }
+            // else if (day > 2 && day < 4)
+            // {
+            //     Console.WriteLine("tuesday");
+            // }
+            // else if (day > 3 && day < 5)
+            // {
+            //     Console.WriteLine("wednesday");
+            // }
+            // else if (day > 4 && day < 6)
+            // {
+            //     Console.WriteLine("thursday");
+            // }
+            // else if (day > 5 && day < 7)
+            // {
+            //     Console.WriteLine("friday");
+            // }
+            // else if(day >6 && day < 8)
+            // {
+            //     Console.WriteLine("saturday");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("your are out of universe");
+            // }
+
+                  // Loops in csharp
+            // while loop
+            // int i = 1;
+            // while (i<5000)
+            // {
+            //     Console.WriteLine(i);
+            //     i++;
+            // }
+
+            // do while loop
+
+            // int  i = 0;
+
+            // do
+            // {
+            //     Console.WriteLine(i + 1);
+            //     i++;
+            // }
+            // while (i < 7);
+
+            // for loops
+            //    for(int i = 0; i<56; i++)
+            //      {
+            //         Console.WriteLine("hello!");
+            // WAP to print 5 times your name.
+
+            // int i = 1;
+            // do
+            // {
+            //     Console.WriteLine(i + ":prashant soti");
+            //     i++;
+            // }
+            // while (i <= 5);
+
+            // //take two number and print their sum.
+            // Console.WriteLine("enter first number:");
+            // double num1 = Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine("enter second number:");
+            // double num2 = Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine("sum of two number:" + (Convert.ToDouble(num1 + num2)));
+            // Console.ReadLine();
+
+
+            // Ask the user for a number and print whether it is even or odd.
+
+            // Console.WriteLine("Enter your number:");
+            // int i = Convert.ToInt32(Console.ReadLine());
+
+            // if (i % 2 == 0)
+            // {
+            //     Console.WriteLine("even");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("odd");
+            // }
+
+            // write a code to make a game like rock paper and segar by taking input by user.
+
+            // User input.
+            // Console.WriteLine("Choose beetwn rock , paper or sissior");
+            // string i = Console.ReadLine();
+            // if ()
+            // {
+            //     Console.WriteLine("invalid input!");
+
+            // mymethod();          
+        }
+    }
+}
